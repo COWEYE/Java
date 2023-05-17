69994e10dc573ed7ce8e03bae89772030492c09f
lee <lee@lee-PC>
2023-05-17 13:03:53 +0900

    주석 추가

--- src/Section01/Main.java
+++ src/Section01/Main.java
@@ -1,16 +1,2 @@
 package Section01;
 
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("문자열을 입력하세요: ");
-        String input = scanner.nextLine();
-
-        System.out.println("입력한 문자열: " + input);
-
-        scanner.close();
-    }
-}
